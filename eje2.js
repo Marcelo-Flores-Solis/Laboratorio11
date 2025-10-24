ea5b25795a215630094ef8dd2a86e3ea4d3adde2
Marcelo-Flores-Solis <mfloresso@unsa.edu.pe>
2025-10-24 16:00:30 -0500

    Paso 4: eje2= edad y resultado

---
+++ eje2.js
@@ -0,0 +1,13 @@
+let edad;
+
+edad = parseInt(prompt("Ingrese su edad:"));
+
+if ( edad >= 60) {
+    console.log("Usted es un adulto mayor.");
+} else if (edad >= 18) {
+        console.log("Usted es un adulto.");
+    } else if (edad >= 13) {
+        console.log("Usted es un adolescente.");
+        } else {
+        console.log("Usted es un niño.");
+        }
